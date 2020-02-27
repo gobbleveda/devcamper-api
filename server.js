7ffc7042017dc5b8a935842cebc6d6c6bf443b8b
[gobbleveda] <[gobbleveda@yahoo.com]>
2020-02-27 15:01:03 -0600

    changed PORT variable

--- server.js
+++ server.js
@@ -110,8 +110,8 @@ app.use('/api/v1/reviews',reviews);
 app.use(errorHandler);
 
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)});
+const LOCALPORT = process.env.PORT || 5000;
+const server = app.listen(LOCALPORT, () => {console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)});
 
 //Handle unhandled promise rejections globally
 
