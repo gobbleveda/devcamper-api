ac5b784a6342c9244dff86001c9a73bb35fb2ddb
[gobbleveda] <[gobbleveda@yahoo.com]>
2020-02-27 15:07:10 -0600

    moved env variables to db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose');
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+}= process.env;
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
